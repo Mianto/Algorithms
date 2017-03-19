35ee90c3dd78a4af57fa0c7560cfbe0319ab0a14
mianto <siddhantshaw97@gmail.com>
2017-03-19 14:47:21 +0530

    Another implementation of BFS added.

--- Graph/bfs.py
+++ Graph/bfs.py
@@ -1,29 +1,47 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+
 
 class Graph:
-	def __init__(self):
-		self.graph = defaultdict(list)
+    def __init__(self):
+        self.graph = defaultdict(list)
 
-	def addEdge(self, vertex, edge):
-		self.graph[vertex].append(edge)
+    def addEdge(self, vertex, edge):
+        self.graph[vertex].append(edge)
 
-	def BFS(self, vertex):
-		visited = [False]*(len(self.graph)+ 3)
+    def BFS(self, vertex):
+        visited = [False] * (len(self.graph) + 3)
 
-		queue = list()
-		queue.append(vertex)
+        queue = list()
+        queue.append(vertex)
 
-		visited[vertex] = True
-		
-		while queue:
-			s = queue.pop(0)
-			print s,
+        visited[vertex] = True
 
-			for x in self.graph[s]:
-				if visited[x]==False:
-					queue.append(x)
-					visited[x] = True
-					
+        while queue:
+            s = queue.pop(0)
+            print s,
+
+            for x in self.graph[s]:
+                if not visited[x]:
+                    queue.append(x)
+                    visited[x] = True
+
+    # Another implementation of bfs without using visited
+    # but returning a list of vertices
+    def bfs_as_connected(self, vertex):
+        queue = deque()
+        queue.append(vertex)
+        explored = []
+
+        while queue:
+            node = queue.popleft()
+            if node not in explored:
+                explored.append(node)
+                neighbours = self.graph[node]
+
+                for neighbour in neighbours:
+                    queue.append(neighbour)
+        return explored
+
 
 g = Graph()
 g.addEdge(1, 2)
@@ -33,3 +51,4 @@ g.addEdge(1, 5)
 g.addEdge(5, 6)
 
 g.BFS(1)
+print g.bfs_as_connected(1)
