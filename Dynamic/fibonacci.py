7f0e25aab167b53c104587f3489dbc77fe3325f9
mianto <siddhantshaw97@gmail.com>
2017-04-10 18:39:55 +0530

    Pep8 formatting

--- Dynamic/fibonacci.py
+++ Dynamic/fibonacci.py
@@ -1,28 +1,31 @@
 # bottom-up
 def fibo(n):
-	arr = [0]*(n+1)
-	arr[1] = 1
-	for i in xrange(2, n+1):
-		arr[i] = arr[i-1]+arr[i-2]
+    arr = [0] * (n + 1)
+    arr[1] = 1
+    for i in xrange(2, n + 1):
+        arr[i] = arr[i - 1] + arr[i - 2]
 
-	return arr[n]
+    return arr[n]
 
 # Top Down
-def fibonacci(n, arr):
-	if n == 0 or n == 1:
-		arr[n] = n
-	
-	if arr[n] is None:
-		arr[n] = fibonacci(n-1, arr)+fibonacci(n-2, arr)
 
-	return arr[n]
+
+def fibonacci(n, arr):
+    if n == 0 or n == 1:
+        arr[n] = n
+
+    if arr[n] is None:
+        arr[n] = fibonacci(n - 1, arr) + fibonacci(n - 2, arr)
+
+    return arr[n]
+
 
 def main():
-	'''n = int(raw_input())
-	print "Fibonacci of the Given Number", fibo(n)'''
-
-	arr = [None]*100
-	print "Fibonacci  of The Given Number", fibonacci(n, arr)
+    '''n = int(raw_input())
+    print "Fibonacci of the Given Number", fibo(n)'''
+    n = int(raw_input())
+    arr = [None] * 100
+    print "Fibonacci  of The Given Number", fibonacci(n, arr)
 
 if __name__ == '__main__':
-	main()
+    main()
--- Dynamic/lincseq.py
+++ Dynamic/lincseq.py
@@ -1,12 +1,12 @@
 def lis(a):
-	n = len(a)
-	li = [1]*n
+    n = len(a)
+    li = [1] * n
 
-	for i in xrange(1, n):
-		for j in xrange(0, i):
-			if a[i] > a[j] and li[i]<li[j]+1:
-				li[i] = li[j] + 1
+    for i in xrange(1, n):
+        for j in xrange(0, i):
+            if a[i] > a[j] and li[i] < li[j] + 1:
+                li[i] = li[j] + 1
 
-	return max(li)
-arr = [ 3, 7, 2, 40, 80]
+    return max(li)
+arr = [3, 7, 2, 40, 80]
 print lis(arr)
